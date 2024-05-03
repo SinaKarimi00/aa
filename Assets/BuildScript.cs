01fdb0baece9dff04969581229be281b30f12d52
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-05-03 18:29:59 +0330

    test1

---
+++ Assets/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+namespace DefaultNamespace
+{
+    public class BuildScript
+    {
+#if UNITY_EDITOR
+
+        public static void BuildAndroid()
+        {
+            string[] scenes = { "Assets/Scene1.unity", "Assets/Scene2.unity" }; // Add your scene paths here
+            string outputPath = "Builds/AndroidBuild.apk"; // Specify the output path for the APK
+
+            BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        }
+#endif
+    }
+}
